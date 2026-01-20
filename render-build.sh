484db52f8cba37d01e2ef6640ab5af04589e975d
Muhammed Furkan Ertürk <138170751+sxwcxz@users.noreply.github.com>
2026-01-21 01:15:44 +0300

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
-# Hata olursa durdur
 set -o errexit
 
 # Python kütüphanelerini kur
 pip install -r requirements.txt
 
-# FFmpeg indir ve kur
+# FFmpeg indir, kur ve İZİN VER
 if [ ! -f ./ffmpeg ]; then
     echo "FFmpeg indiriliyor..."
     wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz
     tar -xvf ffmpeg-release-amd64-static.tar.xz
     mv ffmpeg-*-amd64-static/ffmpeg ./ffmpeg
+    chmod +x ./ffmpeg  # <-- EN ÖNEMLİ KISIM BURASI (Çalıştırma izni)
     rm -rf ffmpeg-*-amd64-static*
-    echo "FFmpeg kuruldu!"
+    echo "FFmpeg kuruldu ve izin verildi!"
 fi
